616e762ddbab22078a5614accd25102c3c2aa870
alphaalpha <14131323@me.mcu.edu.tw>
2026-04-11 16:09:23 +0800

    feat(product): 建立產品卡片清單元件

---
+++ src/app/product-card-list/product-card-list.component.ts
@@ -0,0 +1,20 @@
+import { ProductCardComponent } from './../product-card/product-card.component';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card-list',
+  imports: [ProductCardComponent],
+  templateUrl: './product-card-list.component.html',
+  styleUrl: './product-card-list.component.scss',
+})
+export class ProductCardListComponent {
+  protected productName = '書籍 A';
+  protected author = '作者甲、作者乙、作者丙';
+  protected company = '博碩文化';
+
+  protected isShow = true;
+
+  protected photoUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img2';
+  protected createDate = new Date('2025/4/9');
+  protected price = 10000;
+}
