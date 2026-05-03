c408ad024a76ff458e0f1f8950785c2a44abfcab
alphaalpha <14131323@me.mcu.edu.tw>
2026-05-03 11:28:38 +0800

    feat(product-detail): 切換產品明細頁面實作

--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,6 +1,6 @@
 import { Product } from '../model/product';
 import { ProductCardComponent } from './../product-card/product-card.component';
-import { Component, input, signal } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import { PaginationComponent } from '../pagination/pagination.component';
 @Component({
   selector: 'app-product-card-list',
@@ -10,5 +10,8 @@ import { PaginationComponent } from '../pagination/pagination.component';
 })
 export class ProductCardListComponent {
   readonly products = input<Product[]>([]);
+
+  readonly view = output<Product>();
+
   readonly pageIndex = signal(1);
 }
