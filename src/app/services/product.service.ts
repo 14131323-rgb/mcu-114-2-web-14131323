5d51facc3b12c4efbe0dc22e8b7fd7f4c5c8e54c
alphaalpha <14131323@me.mcu.edu.tw>
2026-05-10 11:29:12 +0800

    feat(product-service): 變更依編號取得產品回傳型別

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../model/product';
-import { delay, Observable, of } from 'rxjs';
+import { delay, filter, mergeMap, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -58,8 +58,12 @@ export class ProductService {
       price: 10000,
     }),
   ];
-  getById(productId: number): Product {
-    return this._data.find(({ id }) => id === productId)!;
+
+  getById(productId: number): Observable<Product> {
+    return of(this._data).pipe(
+      mergeMap((data) => data),
+      filter(({ id }) => id === productId)
+    );
   }
 
   getList(name: string | undefined, index: number, size: number): Observable<{ data: Product[]; count: number }> {
