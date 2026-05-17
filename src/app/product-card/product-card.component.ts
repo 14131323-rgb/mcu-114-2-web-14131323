af8ee12b44d9be34617e0fceea725716383b908b
alphaalpha <14131323@me.mcu.edu.tw>
2026-05-17 15:21:17 +0800

    feat(product): 變更產品編號型別為字串

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -9,7 +9,7 @@ import { booleanAttribute, Component, input, output, model, numberAttribute } fr
   host: { class: 'app-product-card' },
 })
 export class ProductCardComponent {
-  readonly id = input.required<number, string | number>({ transform: numberAttribute });
+  readonly id = input.required<string>();
 
   readonly productName = input<string>();
 
