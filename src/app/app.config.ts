1c39c64268126096065e7623948c0bd7e66c1f24
alphaalpha <14131323@me.mcu.edu.tw>
2026-05-17 12:41:32 +0800

    feat(product): 實作從後端服務取得所有產品資料

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
-
+import { ProductService } from './services/product.service';
 import { routes } from './app.routes';
+import { ProductRemoteService } from './services/product-remote.service';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes, withComponentInputBinding())],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideHttpClient(),
+    provideRouter(routes, withComponentInputBinding()),
+    { provide: ProductService, useClass: ProductRemoteService },
+  ],
 };
